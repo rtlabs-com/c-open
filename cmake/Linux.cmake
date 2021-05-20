0f249e191f4c401ce6f2beb2b08b9086667bd9fb
Hans-Erik Floryd <hans-erik.floryd@rt-labs.com>
2021-05-20 10:59:27 +0200

    Fix USE_SCHED_FIFO option
    USE_SCHED_FIFO was not propagated to the OSAL subproject as
    intended. Bump cmake-tools for newer OSAL which sets USE_SCHED_FIFO
    option locally. Remove local setting of USE_SCHED_FIFO option.

--- cmake/Linux.cmake
+++ cmake/Linux.cmake
@@ -13,14 +13,6 @@
 # full license information.
 #*******************************************************************/
 
-option (USE_SCHED_FIFO
-  "Use SCHED_FIFO policy. May require extra privileges to run"
-  OFF)
-
-if (USE_SCHED_FIFO)
-  add_compile_definitions(USE_SCHED_FIFO)
-endif()
-
 target_include_directories(canopen
   PRIVATE
   src/ports/linux
